d42ae0f92a10b56c4d117c43421777ff82ed4f6e
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-12-16 00:21:13 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🇬🇧○ Creator : <a href='tg://user?id={OWNER_ID}'>Admin</a> </b>\n\n<b>○ Twitter : <a href='https://twitter.com/BFROOMS?t=I9Q2Q4XHXCkYSCx2Aln6XA&s=09'>BFROOMS</a> </b>",
+            text = f"<b>🇬🇧○ Creator : <a href='tg://user?id={OWNER_ID}'>Admin</a> </b>\n\n○ Twitter : <a href='https://twitter.com/BFROOMS?t=I9Q2Q4XHXCkYSCx2Aln6XA&s=09'>BFROOMS</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
